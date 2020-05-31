6dd8bd744674732e7c002b22de18658af716bf5d
Fabricio Parola <fabricio.parola@gmail.com>
2020-05-31 18:20:33 -0300

    fix: show in error message the name of the error

--- src/app/middlewares/error-handler.middleware.ts
+++ src/app/middlewares/error-handler.middleware.ts
@@ -4,7 +4,7 @@ import { AppError } from '../abstracts/app-error.abstract'
 export class ErrorHandlerMiddleware {
   public static async handle (error: AppError, request: Request, response: Response, next: NextFunction): Promise<Response> {
     return response.json({
-      message: error.message,
+      message: error.name,
       status: error.status,
       errors: error.errors
     })
